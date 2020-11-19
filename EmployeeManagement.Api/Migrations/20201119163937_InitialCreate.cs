94b72f64c4a6a09badc2c694643ceb234f8d4f8a
chisoftltd <chisoftltd@gmail.com>
2020-11-19 17:48:51 +0100

    Adding ASP.NET Core REST API DbContext

---
+++ EmployeeManagement.Api/Migrations/20201119163937_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Department",
+                columns: new[] { "DepartmentId", "DepartmentName" },
+                values: new object[,]
+                {
+                    { 1, "IT" },
+                    { 2, "HR" },
+                    { 3, "Payroll" },
+                    { 4, "Admin" },
+                    { 5, "Sales" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "DepartmentId", "Email", "FirstName", "Gender", "LastName", "PhotoPath" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1980, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "benjamin.uchenna@chisoftmedia.com", "Benjamin", 0, "Uchenna", "images/benjamin.png" },
+                    { 2, new DateTime(1984, 5, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "joy.obiageri@chisoftmedia.com", "Joy", 1, "Obiageri", "images/joy.png" },
+                    { 3, new DateTime(2002, 4, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "emma.echerem@chisoftmedia.com", "Emmanuel", 0, "Chinaecherem", "images/emma.png" },
+                    { 4, new DateTime(2012, 12, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), 4, "shepherd.ebuka@chisoftmedia.com", "Shepherd", 0, "Chukwebuka", "images/shepherd.png" },
+                    { 5, new DateTime(2017, 10, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 5, "tobechukwu.mikael@chisoftmedia.com", "Tobechukwu", 0, "Mikael", "images/tobechukwu.png" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ EmployeeManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,103 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Department { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Seed Departments Table
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 1, DepartmentName = "IT" });
+
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 2, DepartmentName = "HR" });
+
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 3, DepartmentName = "Payroll" });
+
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 4, DepartmentName = "Admin" });
+
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 5, DepartmentName = "Sales" });
+
+            //Seed Employee Table
+            modelBuilder.Entity<Employee>().HasData(new Employee 
+            { 
+                EmployeeId = 1,
+                FirstName = "Benjamin",
+                LastName = "Uchenna",
+                Email = "benjamin.uchenna@chisoftmedia.com",
+                DateOfBirth = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                DepartmentId = 1,
+                PhotoPath = "images/benjamin.png"
+
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Joy",
+                LastName = "Obiageri",
+                Email = "joy.obiageri@chisoftmedia.com",
+                DateOfBirth = new DateTime(1984, 05, 25),
+                Gender = Gender.Female,
+                DepartmentId = 2,
+                PhotoPath = "images/joy.png"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Emmanuel",
+                LastName = "Chinaecherem",
+                Email = "emma.echerem@chisoftmedia.com",
+                DateOfBirth = new DateTime(2002, 04, 15),
+                Gender = Gender.Male,
+                DepartmentId = 3,
+                PhotoPath = "images/emma.png"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "Shepherd",
+                LastName = "Chukwebuka",
+                Email = "shepherd.ebuka@chisoftmedia.com",
+                DateOfBirth = new DateTime(2012, 12, 25),
+                Gender = Gender.Male,
+                DepartmentId = 4,
+                PhotoPath = "images/shepherd.png"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 5,
+                FirstName = "Tobechukwu",
+                LastName = "Mikael",
+                Email = "tobechukwu.mikael@chisoftmedia.com",
+                DateOfBirth = new DateTime(2017, 10, 2),
+                Gender = Gender.Male,
+                DepartmentId = 5,
+                PhotoPath = "images/tobechukwu.png"
+            });
+        }
+    }
+}
