4598864452ca7b3d9ef89bc134dbd06e9527fa47
Benjamin Chinwe <chisoftltd@dawnfresh.co.uk>
2020-11-22 22:34:16 +0100

    Implementation of Route Parameters, Display data from two tables and event handling

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,40 @@ namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeDetailsBase : ComponentBase
     {
+        protected string Coordinates { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
         public Employee Employee { get; set; } = new Employee();
+        
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
-        [Parameter]
+        
+        [Parameter]        
         public string Id { get; set; }
-
+        
         protected async override Task OnInitializedAsync()
         {
             Id = Id ?? "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+
+        //protected void Mouse_Move(MouseEventArgs e)
+        //{
+        //    Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        //}
+
+        protected void Button_Click()
+        {
+            if (ButtonText=="Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
         }
     }
 }
--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -1,7 +1,6 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.services;
 using Microsoft.AspNetCore.Components;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,12 +10,12 @@ namespace EmployeeManagement.Web.Pages
     public class EmployeeListBase : ComponentBase
     {
         [Inject]
-        public IEmployeeService EmployeeServices { get; set; }
+        public IEmployeeService EmployeeService { get; set; }
         public IEnumerable<Employee> Employees { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            Employees = (await EmployeeServices.GetEmployees()).ToList();
+            Employees = (await EmployeeService.GetEmployees()).ToList();
         }
     }
 }
