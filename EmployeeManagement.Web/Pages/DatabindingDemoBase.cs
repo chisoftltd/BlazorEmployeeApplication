2df78bd1dbe3d2d904fe271b44cbdcc0dd76ea7e
Benjamin Chinwe <chisoftltd@dawnfresh.co.uk>
2020-11-23 20:51:46 +0100

    Implementing Data binding in Blazor

---
+++ EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "Tom";
+        protected string Gender { get; set; } = "Male";
+    }
+}
